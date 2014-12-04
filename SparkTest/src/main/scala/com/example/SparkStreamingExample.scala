f9d7a3359a36a647034003424ca742a041eaa9d2
Yun Liaw <frouscy@gmail.com>
2014-12-05 07:37:53 +0800

    add twitter streaming template

--- SparkTest/src/main/scala/com/example/SparkStreamingExample.scala
+++ SparkTest/src/main/scala/com/example/SparkStreamingExample.scala
@@ -6,13 +6,22 @@ import org.apache.spark.streaming.StreamingContext._
 
 object SparkStreamingExample {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(2))
+    ssc.checkpoint("./checkpoint")
+
+    val updateFunc = (values: Seq[Int], state: Option[Int]) => {
+      val currentCount = values.foldLeft(0)(_ + _)
+      val previousCount = state.getOrElse(0)
+      Some(currentCount + previousCount)
+    }
 
     val lines = ssc.socketTextStream("localhost", 9999)
     val words = lines.flatMap(_.split(" "))
     val pairs = words.map(word => (word, 1))
-    val wordCounts = pairs.reduceByKey(_ + _)
+    val wordCounts = pairs.updateStateByKey[Int](updateFunc)
     wordCounts.print()
 
     ssc.start()
