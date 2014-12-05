ab6f752add2c8490148ae14466cc6680a85155eb
Yun Liaw <frouscy@gmail.com>
2014-12-05 09:07:14 +0800

    gitignore typo

--- streaming/src/main/scala/streaming/Tutorial.scala
+++ streaming/src/main/scala/streaming/Tutorial.scala
@@ -1,20 +1,45 @@
 package streaming
 
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.SparkContext._
+
 object Tutorial {
   def main(args: Array[String]) {
-    
     // Checkpoint directory
     val checkpointDir = TutorialHelper.getCheckpointDirectory()
 
     // Configure Twitter credentials
-    val apiKey = ""
-    val apiSecret = ""
-    val accessToken = ""
-    val accessTokenSecret = ""
+    val apiKey = "umhLxAaxpcfQrAksVfwl8rcKw"
+    val apiSecret = "yAD1GZecWpFWGSd0kehGMweToRDNAW1xaGsHzi3bObMagLkyFu"
+    val accessToken = "22887707-Sc0SMMJNd7EggYCXMyq4OfQl5V1ZdCCHSX2UR4BtV"
+    val accessTokenSecret = "SbHsQou18OVzYlFrDUbaBdohJtzwPxCAL6ZCsOw"
     TutorialHelper.configureTwitterCredentials(apiKey, apiSecret, accessToken, accessTokenSecret)
 
     // Your code goes here
 
+    val ssc = new StreamingContext(new SparkConf().setMaster("local[2]").setAppName("Streaming"), Seconds(1))
+
+    val tweets = TwitterUtils.createStream(ssc, None)
+
+    val statuses = tweets.map(status => status.getText())
+    val words = statuses.flatMap(status => status.split(" "))
+    val hashtags = words.filter(word => word.startsWith("#"))
+    val counts = hashtags
+      .map(tag => (tag, 1))
+      .reduceByKeyAndWindow(_ + _, _ - _, Seconds(60 * 5), Seconds(1))
+    val sortedCounts = counts
+      .map { case(tag, count) => (count, tag) }
+      .transform(rdd => rdd.sortByKey(ascending = false))
+    sortedCounts.foreach(rdd =>
+      println("\nTop 10 hashtags:\n" + rdd.take(10).mkString("\n")))
+
+    ssc.checkpoint(checkpointDir)
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
 
